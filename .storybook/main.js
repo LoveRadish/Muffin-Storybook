cac8e3fbe27006676a9136a162561d4ef9cd8119
Alexandr1227 <Alexandr_1227@outlook.com>
2021-05-04 22:04:59 -0700

    add tasklist

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  stories: ['../src/components/Task.stories.js','../src/components/TaskList.stories.js'],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
